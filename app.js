3d5f2893956508268c7664315793b42efff2d921
el_bomba <santi_ha97@hotmail.com>
2022-06-22 12:21:40 +0100

    versión estática local con React

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
-const app = express()
 
+const app = express()
+const port = process.env.PORT;
 
 // Contenido estático
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     // Cuando quieran acceder al inicio ocultamos el .html
     // Que también funciona al acceder por el header
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`Example listening at http://localhost:${port}`)
+});
